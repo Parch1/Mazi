3c618d6b4ace2044d0fcb5eb814f70927334fa1d
Ryan Lee <ryanlee7132@gmail.com>
2020-04-15 19:58:27 -0400

    Migrate header navigation to footer

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
 
 const tasks = [
   {
@@ -24,7 +28,6 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-
       <Button
         title="Home"
         onPress={() => navigation.navigate('Home')}
@@ -44,7 +47,6 @@ function HomeScreen({ navigation }) {
         title="Profile"
         onPress={() => navigation.navigate('Profile')}
       />
-
     </View>
   );
 }
@@ -142,14 +144,33 @@ function ProfileScreen({ navigation }) {
   )
 }
 
-const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#e91e63"
+        style={{ backgroundColor: 'tomato' }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Tasks" component={TasksScreen} />
+        <Tab.Screen name="Rewards" component={RewardsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
 
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
 
+export default App;
+
+/*
 // Routes
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+
+      <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
           headerStyle: { backgroundColor: '#f4511e', },
@@ -171,8 +192,8 @@ function App() {
         <Stack.Screen name="Rewards" component={RewardsScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
-
-export default App;
+*/
